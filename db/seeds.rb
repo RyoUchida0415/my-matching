36bd7f1bc302680561166f5a02bda311ab2f9df3
vagrant <vagrant@localhost.localdomain>
2020-09-07 05:54:10 +0000

    add blog new action

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ User.create!(
    nickname: 'ふーたろう',
    email: '1@1',
    age:'27',
-   prefecture:'大阪府',
-   sex:'男',
+   prefecture:'1',
+   sex:'1',
    password:'111111',
    password_confirmation:'111111'
 )
