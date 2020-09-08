3362b7d0739c3aea769a8fb356a16fb542834380
ryouchida0415 <tkkndshr2565@gmail.com>
2020-09-08 15:10:37 +0000

    add follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
-  devise_for :users
-  resources :users
-  resources :blogs
-  resources :relationships
-  root "homes#top"
-  end
+	get 'homes/top'
+	get 'homes/about'
+	devise_for :users
+	resources :users do
+		resource :relationships, only: [:create, :destroy]
+		get 'follows' => 'relationships#follower', as: 'follows'
+		get 'followers' => 'relationships#followed', as: 'followers'
+	end
+	resources :relationships
+	resources :blogs
+	root "homes#top"
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_024158) do
+ActiveRecord::Schema.define(version: 2020_09_08_125041) do
 
   create_table "blogs", force: :cascade do |t|
     t.integer "user_id"
@@ -20,10 +20,10 @@ ActiveRecord::Schema.define(version: 2020_09_07_024158) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "follow_id"
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "follower_id"
   end
 
   create_table "users", force: :cascade do |t|
