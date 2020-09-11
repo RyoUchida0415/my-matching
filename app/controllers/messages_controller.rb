4fcfc89b6c3d48e5c57df2d092c7f07244e16fb1
ryouchida0415 <tkkndshr2565@gmail.com>
2020-09-11 11:56:28 +0000

    matching kinou

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
 	before_action :authenticate_user!, :only => [:create]
 
 	def create
+		#rooms/show.html.erbで送られてきたform_forで送られてきたcontentを含む全てのメッセージの情報の:messageと:room_idのキーが存在しているならば
 		if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
 			@message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
 			redirect_to "/rooms/#{@message.room_id}"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,40 @@
 class UsersController < ApplicationController
   def index
         #別の性別を表示させるようにしたい
-        if User.where(sex: true)
-          @users = User.where(sex: false)
-        elsif User.where(sex: false)
-          @users = User.where(sex: true)
-        end
+        #if User.where(:sex => 'true')
+          #@users = User.where(sex: 'false')
+        #elsif User.where(:sex => 'false')
+          #@users = User.where(sex: 'true')
+        #end
+        @users = User.all
       end
 
       def show
         @user = User.find(params[:id])
         @blogs = Blog.where(user_id: @user.id)
 
-    #メッセージ機能
-    @currentUserEntry=Entry.where(user_id: current_user.id)
-    @userEntry=Entry.where(user_id: @user.id)
-    #現在ログインしているユーザーでないのならば
-    unless @user.id == current_user.id
-      @currentUserEntry.each do |current_user|
-        @userEntry.each do |user|
-          #roomが作成されているならば
-          if current_user.room_id == user.room_id then
-            @isRoom = true
-            @roomId = current_user.room_id
+        #メッセージ機能
+        #現在ログインしているユーザーと相手のユーザを探している
+        @currentUserEntry=Entry.where(user_id: current_user.id)
+        @userEntry=Entry.where(user_id: @user.id)
+        #現在ログインしているユーザーでないならば
+        unless @user.id == current_user.id
+          @currentUserEntry.each do |current_user|
+            @userEntry.each do |user|
+              #現在ログインしているユーザーのroom_idと相手のユーザーのroom_idが同じならば
+              if current_user.room_id == user.room_id then
+                @isRoom = true
+                @roomId = current_user.room_id
+              end
+            end
+          end
+          #roomが作成されていないならば
+          unless @isRoom
+            @room = Room.new
+            @entry = Entry.new
           end
         end
       end
-      unless @isRoom
-        @room = Room.new
-        @entry = Entry.new
-      end
-    end
-  end
 
   def edit
   end
