e789454523aea99a1c5577137308789cf521cc22
ryouchida0415 <tkkndshr2565@gmail.com>
2020-09-13 17:19:16 +0000

    css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,39 +9,38 @@ class UsersController < ApplicationController
         #全ての男性会員を表示
         @users = User.where(sex: true)
       end
-
   end
 
-      def show
-        @user = User.find(params[:id])
-        @blogs = Blog.where(user_id: @user.id)
+  def show
+    @user = User.find(params[:id])
+    @blogs = Blog.where(user_id: @user.id)
 
-        #メッセージ機能
-        #現在ログインしているユーザーと相手のユーザを探している
-        @currentUserEntry=Entry.where(user_id: current_user.id)
-        @userEntry=Entry.where(user_id: @user.id)
-        #現在ログインしているユーザーでないならば
-        unless @user.id == current_user.id
-          @currentUserEntry.each do |current_user|
-            @userEntry.each do |user|
-              #現在ログインしているユーザーのroom_idと相手のユーザーのroom_idが同じならば
-              if current_user.room_id == user.room_id then
-                @isRoom = true
-                @roomId = current_user.room_id
-              end
-            end
-          end
-          #roomが作成されていないならば
-          unless @isRoom
-            @room = Room.new
-            @entry = Entry.new
+    #メッセージ機能
+    #現在ログインしているユーザーと相手のユーザを探している
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    #現在ログインしているユーザーでないならば
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |current_user|
+        @userEntry.each do |user|
+          #現在ログインしているユーザーのroom_idと相手のユーザーのroom_idが同じならば
+          if current_user.room_id == user.room_id then
+            @isRoom = true
+            @roomId = current_user.room_id
           end
         end
       end
-
-  def edit
+      #roomが作成されていないならば
+      unless @isRoom
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
-  def update
-  end
-end
+      def edit
+      end
+
+      def update
+      end
+    end
