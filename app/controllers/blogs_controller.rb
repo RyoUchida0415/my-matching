3ecdb166296014af0ed441e4fc6aea87d2e268b9
ryouchida0415 <tkkndshr2565@gmail.com>
2020-09-16 07:23:14 +0000

    validates user/edit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,11 @@ class BlogsController < ApplicationController
 	def create
 		@blog = Blog.new(blog_params)
 		@blog.user_id = current_user.id
-		@blog.save
-		redirect_to users_path
+		if @blog.save
+		  redirect_to user_path(@user.id)
+		else
+		  render 'new'
+		end
 	end
 
 	private
